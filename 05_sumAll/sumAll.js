71ec6143cfc19330f6a981049fdb268d11a9499f
Massa Timothée <timothee.massa.perso@gmail.com>
2023-01-29 17:13:28 +0100

    Finished only by looking how to determine wether a given argument is a  number or not

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
+    let finalResult = 0;
+
+    if ((number1 || number2) < 0) {
+        return 'ERROR';
+    } else if (!Number.isInteger(number1) || !Number.isInteger(number2)) {
+        return 'ERROR';
+    }
+    
+    if (number1 < number2) {
+        for (let i = number1; i <= number2; i++) {
+            finalResult += i;
+        }
+    } else {
+        for (let i = number1; i >= number2; i--) {
+            finalResult += i;
+            console.log(finalResult);
+        }
+    }
+
+    return finalResult;
 };
 
 // Do not edit below this line
